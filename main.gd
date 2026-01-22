e917200dde97eb964aa9dbd2ba64698eb6f75f54
MrSkillFull <fernandoassisoliveiralima@gmail.com>
2026-01-21 22:51:39 -0300

    sincronizando

--- main.gd
+++ main.gd
@@ -4,14 +4,24 @@ var patosNaTela
 var pato = preload("res://pato.tscn")
 
 func _ready() -> void:
-	pass
+	$GeraPato.start()
 	
 func _process(delta: float) -> void:
 	$Alvo.position.x = get_local_mouse_position().x
 	$Alvo.position.y = get_local_mouse_position().y
 
+func Nasce() -> void:
+	var novoPato = pato.instantiate()
+	add_sibling(novoPato)
+	novoPato.position.x = randi_range(10,700)
+	novoPato.position.y = 700
+	
 
 func _on_gera_pato_timeout() -> void:
 	patosNaTela = randi_range(1, 6)
 	for n in patosNaTela:
 		Nasce()
+
+
+func _on_espera_timeout() -> void:
+	$GeraPato.start()
